1171025bda61e17f5b5c8f57d0b5194753c1af41
raprmdn <rafiputraramadhan14@gmail.com>
2022-10-20 14:13:07 +0700

    registration test

--- __test__/integration-testing/test.usergame.spec.js
+++ __test__/integration-testing/test.usergame.spec.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 
 describe('Integration Testing - UserGame API Territory', () => {
 
-    it('POST /api/auth/login : Should be login "The given data was invalid."', async () => {
+    it('POST /api/auth/login : Should be login error "The given data was invalid."', async () => {
         const res = await request(app)
             .post('/api/auth/login')
             .set('Content-Type', 'application/json')
@@ -20,7 +20,7 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body).to.have.property('errors');
     });
 
-    it('POST /api/auth/login : Should be login "These credentials do not match our records"', async () => {
+    it('POST /api/auth/login : Should be login error "These credentials do not match our records"', async () => {
         const res = await request(app)
             .post('/api/auth/login')
             .set('Content-Type', 'application/json')
@@ -33,7 +33,7 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body.message).to.be.equal('These credentials do not match our records');
     });
 
-    it('POST /api/auth/login : Should be login "User logged in successfully"', async () => {
+    it('POST /api/auth/login : Should be login success "User logged in successfully"', async () => {
         const res = await request(app)
             .post('/api/auth/login')
             .set('Content-Type', 'application/json')
@@ -52,5 +52,78 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body.data.user).to.have.property('token');
     });
 
+    it('POST /api/auth/register : Should be register error "The given data was invalid."', async () => {
+        const res = await request(app)
+            .post('/api/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({ 'name': '', 'username': '', 'email': '', 'password': '', 'password_confirmation': '' });
 
+        expect(res.statusCode).to.equal(422);
+
+        expect(res.body.status).to.be.equal(422);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('The given data was invalid.');
+        expect(res.body).to.have.property('errors');
+    });
+
+    it('POST /api/auth/register : Should be register error "Username already exists"', async () => {
+        const res = await request(app)
+            .post('/api/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({
+                'name': "Rafi Putra Ramadhan",
+                'username': 'raprmdn',
+                'email': 'raprmdn@gmail.com',
+                'password': 'Abc123456!',
+                'password_confirmation': 'Abc123456!'
+            });
+
+        expect(res.statusCode).to.equal(422);
+
+        expect(res.body.status).to.be.equal(422);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('The given data was invalid.');
+        expect(res.body).to.have.property('errors');
+        expect(res.body.errors.username).to.be.equal('Username already exists');
+    });
+
+    it('POST /api/auth/register : Should be register error "Email already exists"', async () => {
+        const res = await request(app)
+            .post('/api/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({
+                'name': "Rafi Putra Ramadhan",
+                'username': 'raprmdnn',
+                'email': 'raprmdn@gmail.com',
+                'password': 'Abc123456!',
+                'password_confirmation': 'Abc123456!'
+            });
+
+        expect(res.statusCode).to.equal(422);
+
+        expect(res.body.status).to.be.equal(422);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('The given data was invalid.');
+        expect(res.body).to.have.property('errors');
+        expect(res.body.errors.email).to.be.equal('Email already exists');
+    });
+
+    it('POST /api/auth/register : Should be register success "User registered successfully"', async () => {
+        const res = await request(app)
+            .post('/api/auth/register')
+            .set('Content-Type', 'application/json')
+            .send({
+                'name': "Rafi Putra Ramadhan",
+                'username': 'raprmdnn',
+                'email': 'raprmdnn@email.com',
+                'password': 'Abc123456!',
+                'password_confirmation': 'Abc123456!'
+            });
+
+        expect(res.statusCode).to.be.equal(201);
+
+        expect(res.body.status).to.be.equal(201);
+        expect(res.body.success).to.be.equal(true);
+        expect(res.body.message).to.be.equal('User registered successfully');
+    });
 });
