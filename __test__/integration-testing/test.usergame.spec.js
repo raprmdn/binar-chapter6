bcd88704f5a820a07509d898195aad122047d521
raprmdn <rafiputraramadhan14@gmail.com>
2022-10-20 15:52:27 +0700

    UserGame test.

--- __test__/integration-testing/test.usergame.spec.js
+++ __test__/integration-testing/test.usergame.spec.js
@@ -4,26 +4,39 @@ const app = require('../../src/config/server.config');
 
 const expect = chai.expect;
 
+let token;
+
+const unprocessableEntity = (res) => {
+    expect(res.statusCode).to.equal(422);
+
+    expect(res.body.status).to.be.equal(422);
+    expect(res.body.success).to.be.equal(false);
+    expect(res.body.message).to.be.equal('The given data was invalid.');
+    expect(res.body).to.have.property('errors');
+};
+
+const okResponse = (res) => {
+    expect(res.statusCode).to.equal(200);
+
+    expect(res.body.status).to.be.equal(200);
+    expect(res.body.success).to.be.equal(true);
+};
+
 describe('Integration Testing - UserGame API Territory', () => {
 
-    it('POST /api/auth/login : Should be login error "The given data was invalid."', async () => {
+    it('[POST]  /api/auth/login : Should be login error "The given data was invalid."', async () => {
         const res = await request(app)
             .post('/api/auth/login')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({ 'email': '', 'password': '' });
 
-        expect(res.statusCode).to.equal(422);
-
-        expect(res.body.status).to.be.equal(422);
-        expect(res.body.success).to.be.equal(false);
-        expect(res.body.message).to.be.equal('The given data was invalid.');
-        expect(res.body).to.have.property('errors');
+        unprocessableEntity(res);
     });
 
-    it('POST /api/auth/login : Should be login error "These credentials do not match our records"', async () => {
+    it('[POST]  /api/auth/login : Should be login error "These credentials do not match our records"', async () => {
         const res = await request(app)
             .post('/api/auth/login')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({ 'email': 'raprmdn@gmail.com', 'password': 'Abc123456' });
 
         expect(res.statusCode).to.be.equal(404);
@@ -33,16 +46,13 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body.message).to.be.equal('These credentials do not match our records');
     });
 
-    it('POST /api/auth/login : Should be login success "User logged in successfully"', async () => {
+    it('[POST]  /api/auth/login : Should be login success "User logged in successfully"', async () => {
         const res = await request(app)
             .post('/api/auth/login')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({ 'email': 'raprmdn@gmail.com', 'password': 'Abc123456!' });
 
-        expect(res.statusCode).to.be.equal(200);
-
-        expect(res.body.status).to.be.equal(200);
-        expect(res.body.success).to.be.equal(true);
+        okResponse(res);
         expect(res.body.message).to.be.equal('User logged in successfully');
         expect(res.body.data).to.have.property('user');
         expect(res.body.data.user).to.have.property('id');
@@ -50,26 +60,23 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body.data.user).to.have.property('username');
         expect(res.body.data.user).to.have.property('email');
         expect(res.body.data.user).to.have.property('token');
+
+        token = res.body.data.user.token;
     });
 
-    it('POST /api/auth/register : Should be register error "The given data was invalid."', async () => {
+    it('[POST]  /api/auth/register : Should be register error "The given data was invalid."', async () => {
         const res = await request(app)
             .post('/api/auth/register')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({ 'name': '', 'username': '', 'email': '', 'password': '', 'password_confirmation': '' });
 
-        expect(res.statusCode).to.equal(422);
-
-        expect(res.body.status).to.be.equal(422);
-        expect(res.body.success).to.be.equal(false);
-        expect(res.body.message).to.be.equal('The given data was invalid.');
-        expect(res.body).to.have.property('errors');
+        unprocessableEntity(res);
     });
 
-    it('POST /api/auth/register : Should be register error "Username already exists"', async () => {
+    it('[POST]  /api/auth/register : Should be register error "Username already exists"', async () => {
         const res = await request(app)
             .post('/api/auth/register')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({
                 'name': "Rafi Putra Ramadhan",
                 'username': 'raprmdn',
@@ -78,19 +85,14 @@ describe('Integration Testing - UserGame API Territory', () => {
                 'password_confirmation': 'Abc123456!'
             });
 
-        expect(res.statusCode).to.equal(422);
-
-        expect(res.body.status).to.be.equal(422);
-        expect(res.body.success).to.be.equal(false);
-        expect(res.body.message).to.be.equal('The given data was invalid.');
-        expect(res.body).to.have.property('errors');
+        unprocessableEntity(res);
         expect(res.body.errors.username).to.be.equal('Username already exists');
     });
 
-    it('POST /api/auth/register : Should be register error "Email already exists"', async () => {
+    it('[POST]  /api/auth/register : Should be register error "Email already exists"', async () => {
         const res = await request(app)
             .post('/api/auth/register')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({
                 'name': "Rafi Putra Ramadhan",
                 'username': 'raprmdnn',
@@ -99,19 +101,14 @@ describe('Integration Testing - UserGame API Territory', () => {
                 'password_confirmation': 'Abc123456!'
             });
 
-        expect(res.statusCode).to.equal(422);
-
-        expect(res.body.status).to.be.equal(422);
-        expect(res.body.success).to.be.equal(false);
-        expect(res.body.message).to.be.equal('The given data was invalid.');
-        expect(res.body).to.have.property('errors');
+        unprocessableEntity(res);
         expect(res.body.errors.email).to.be.equal('Email already exists');
     });
 
-    it('POST /api/auth/register : Should be register success "User registered successfully"', async () => {
+    it('[POST]  /api/auth/register : Should be register success "User registered successfully"', async () => {
         const res = await request(app)
             .post('/api/auth/register')
-            .set('Content-Type', 'application/json')
+            .set({ 'Content-Type': 'application/json' })
             .send({
                 'name': "Rafi Putra Ramadhan",
                 'username': 'raprmdnn',
@@ -125,5 +122,85 @@ describe('Integration Testing - UserGame API Territory', () => {
         expect(res.body.status).to.be.equal(201);
         expect(res.body.success).to.be.equal(true);
         expect(res.body.message).to.be.equal('User registered successfully');
+    });
+
+    it('[GET]   /api/auth/me : Should be get user error "Unauthorized"', async () => {
+        const res = await request(app)
+            .get('/api/auth/me')
+            .set({ 'Content-Type': 'application/json' });
+
+        expect(res.statusCode).to.be.equal(401);
+
+        expect(res.body.status).to.be.equal(401);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('Unauthorized');
+    });
+
+    it('[GET]   /api/auth/me : Should be get user success "User retrieved successfully"', async () => {
+        const res = await request(app)
+            .get('/api/auth/me')
+            .set({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` });
+
+        okResponse(res);
+        expect(res.body.success).to.be.equal(true);
+        expect(res.body.message).to.be.equal('User retrieved successfully');
+        expect(res.body.data).to.have.property('user');
+        expect(res.body.data.user).to.have.property('id');
+        expect(res.body.data.user).to.have.property('name');
+        expect(res.body.data.user).to.have.property('username');
+        expect(res.body.data.user).to.have.property('email');
+    });
+
+    it('[PATCH] /api/auth/change-password : Should be change password error "The given data was invalid."', async () => {
+        const res = await request(app)
+            .patch('/api/auth/change-password')
+            .set({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` })
+            .send({ 'current_password': '', 'new_password': '', 'password_confirmation': '' });
+
+        unprocessableEntity(res);
+    });
+
+    it('[PATCH] /api/auth/change-password : Should be change password error "Wrong current password"', async () => {
+        const res = await request(app)
+            .patch('/api/auth/change-password')
+            .set({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` })
+            .send({ 'current_password': 'Abc123456', 'new_password': 'Abc123456!', 'password_confirmation': 'Abc123456!' });
+
+        expect(res.statusCode).to.be.equal(422);
+
+        expect(res.body.status).to.be.equal(422);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('Wrong current password');
+    });
+
+    it('[PATCH] /api/auth/change-password : Should be change password success "Password changed successfully"', async () => {
+        const res = await request(app)
+            .patch('/api/auth/change-password')
+            .set({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` })
+            .send({ 'current_password': 'Abc123456!', 'new_password': 'Abc123456!', 'password_confirmation': 'Abc123456!' });
+
+        okResponse(res);
+        expect(res.body.message).to.be.equal('Password changed successfully');
+    });
+
+    it('[POST]  /api/auth/logout : Should be logout error "Unauthorized"', async () => {
+        const res = await request(app)
+            .post('/api/auth/logout')
+            .set({ 'Content-Type': 'application/json' });
+
+        expect(res.statusCode).to.be.equal(401);
+
+        expect(res.body.status).to.be.equal(401);
+        expect(res.body.success).to.be.equal(false);
+        expect(res.body.message).to.be.equal('Unauthorized');
+    });
+
+    it('[POST]  /api/auth/logout : Should be logout success "Logged out successfully"', async () => {
+        const res = await request(app)
+            .post('/api/auth/logout')
+            .set({ 'Content-Type': 'application/json', 'Authorization': `Bearer ${token}` });
+
+        okResponse(res);
+        expect(res.body.message).to.be.equal('Logged out successfully');
     });
 });
